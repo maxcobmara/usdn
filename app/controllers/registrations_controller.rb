40867a0b0abd5c73c00e04777ea115bcf34da4e3
Yaacob Noor <maxcobmara@gmail.com>
2014-12-29 00:13:14 +0800

    allow company reg

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,7 +13,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    allow = [:email, :password, :password_confirmations, [organisation_attributes: [:name]]
+    allow = [:email, :password, :password_confirmation, :organisation_id, [organisation_attributes: [:id, :name]]]
     params.require(resource_name).permit(allow)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :organisations
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :users
 
   devise_scope :user do
