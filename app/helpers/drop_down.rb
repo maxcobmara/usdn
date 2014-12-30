6ddfa6cbdcedac2f6d19ae51d3e1797e681b1c4a
Yaacob Noor <maxcobmara@gmail.com>
2014-12-30 19:18:40 +0800

    update organisation page

---
+++ app/helpers/drop_down.rb
@@ -0,0 +1,10 @@
+module DropDown
+
+  SITE_CONFIG = [
+    #  Displayed       stored in db
+    [ "Campus",             1 ],
+    [ "Service Provider",   2 ],
+    [ "Domain Controller",  3 ]
+  ]
+
+end
